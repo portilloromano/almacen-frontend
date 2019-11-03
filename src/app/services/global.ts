e23955da0813bde1e12c1b09ebc351efe9a47642
Luis Portillo <portilloromano@gmail.com>
2019-11-03 04:10:46 +0100

    Se cambio el path de las imagenes

--- src/app/services/global.ts
+++ src/app/services/global.ts
@@ -1,7 +1,5 @@
 import { isDevMode } from '@angular/core';
 
 export var global = {
-    url: (isDevMode) ?
-        'http://127.0.0.1:8000' :
-        'https://api-almacen-symfony.herokuapp.com'
+    url: 'http://127.0.0.1:8000'
 }
